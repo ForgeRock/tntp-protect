bb113cbb6a8b171a4a36bb5aeb76920c6614fb13
justinchinfr <justin.chin@forgerock.com>
2023-12-14 14:01:07 -0500

    Legal updates

--- src/main/java/org/forgerock/am/tn/protect/P1ProtectResult.java
+++ src/main/java/org/forgerock/am/tn/protect/P1ProtectResult.java
@@ -1,3 +1,9 @@
+/*
+ * This code is to be used exclusively in connection with ForgeRock’s software or services. 
+ * ForgeRock only offers ForgeRock software or services to legal entities who have entered 
+ * into a binding license agreement with ForgeRock. 
+ */
+
 package org.forgerock.am.tn.protect;
 
 import java.io.BufferedReader;
